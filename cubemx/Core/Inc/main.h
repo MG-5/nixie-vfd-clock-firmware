3e319e86d27fd9f198bdfdc3de5d1676cceaad73
MG-5 <MG-5@users.noreply.github.com>
2024-04-09 23:33:31 +0200

    Generate CubeMX files

---
+++ cubemx/Core/Inc/main.h
@@ -0,0 +1,111 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2024 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f1xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define Enable50V_190V_Pin GPIO_PIN_13
+#define Enable50V_190V_GPIO_Port GPIOC
+#define Dots_Pin GPIO_PIN_14
+#define Dots_GPIO_Port GPIOC
+#define TimeSync_Pin GPIO_PIN_15
+#define TimeSync_GPIO_Port GPIOC
+#define Tube3_Pin GPIO_PIN_0
+#define Tube3_GPIO_Port GPIOD
+#define Tube5_Pin GPIO_PIN_1
+#define Tube5_GPIO_Port GPIOD
+#define Digit0_EnableBusARGB_Pin GPIO_PIN_0
+#define Digit0_EnableBusARGB_GPIO_Port GPIOA
+#define Digit1_EnableBusShiftRegister_Pin GPIO_PIN_1
+#define Digit1_EnableBusShiftRegister_GPIO_Port GPIOA
+#define Digit2_Blanking_Pin GPIO_PIN_2
+#define Digit2_Blanking_GPIO_Port GPIOA
+#define Digit3_Strobe_Pin GPIO_PIN_3
+#define Digit3_Strobe_GPIO_Port GPIOA
+#define Digit4_Heatwire_Pin GPIO_PIN_4
+#define Digit4_Heatwire_GPIO_Port GPIOA
+#define Digit5_Pin GPIO_PIN_5
+#define Digit5_GPIO_Port GPIOA
+#define Digit6_Pin GPIO_PIN_6
+#define Digit6_GPIO_Port GPIOA
+#define Digit7_Pin GPIO_PIN_7
+#define Digit7_GPIO_Port GPIOA
+#define Tube0_Pin GPIO_PIN_10
+#define Tube0_GPIO_Port GPIOB
+#define Tube1_Pin GPIO_PIN_11
+#define Tube1_GPIO_Port GPIOB
+#define Tube2_Pin GPIO_PIN_12
+#define Tube2_GPIO_Port GPIOB
+#define Tube4_Pin GPIO_PIN_14
+#define Tube4_GPIO_Port GPIOB
+#define Digit8_Pin GPIO_PIN_8
+#define Digit8_GPIO_Port GPIOA
+#define Digit9_Pin GPIO_PIN_9
+#define Digit9_GPIO_Port GPIOA
+#define LeftComma_Pin GPIO_PIN_4
+#define LeftComma_GPIO_Port GPIOB
+#define RightComma_Pin GPIO_PIN_5
+#define RightComma_GPIO_Port GPIOB
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
---
+++ cubemx/Core/Src/gpio.c
@@ -0,0 +1,127 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file    gpio.c
+  * @brief   This file provides code for the configuration
+  *          of all used GPIO pins.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2024 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "gpio.h"
+
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/*----------------------------------------------------------------------------*/
+/* Configure GPIO                                                             */
+/*----------------------------------------------------------------------------*/
+/* USER CODE BEGIN 1 */
+
+/* USER CODE END 1 */
+
+/** Configure pins as
+        * Analog
+        * Input
+        * Output
+        * EVENT_OUT
+        * EXTI
+        * Free pins are configured automatically as Analog (this feature is enabled through
+        * the Code Generation settings)
+*/
+void MX_GPIO_Init(void)
+{
+
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOD_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOC, Enable50V_190V_Pin|Dots_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOD, Tube3_Pin|Tube5_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, Digit0_EnableBusARGB_Pin|Digit1_EnableBusShiftRegister_Pin|Digit2_Blanking_Pin|Digit3_Strobe_Pin
+                          |Digit4_Heatwire_Pin|Digit5_Pin|Digit6_Pin|Digit7_Pin
+                          |Digit8_Pin|Digit9_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, Tube0_Pin|Tube1_Pin|Tube2_Pin|Tube4_Pin
+                          |LeftComma_Pin|RightComma_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : PCPin PCPin */
+  GPIO_InitStruct.Pin = Enable50V_190V_Pin|Dots_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : PtPin */
+  GPIO_InitStruct.Pin = TimeSync_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(TimeSync_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PDPin PDPin */
+  GPIO_InitStruct.Pin = Tube3_Pin|Tube5_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PAPin PAPin PAPin PAPin
+                           PAPin PAPin PAPin PAPin
+                           PAPin PAPin */
+  GPIO_InitStruct.Pin = Digit0_EnableBusARGB_Pin|Digit1_EnableBusShiftRegister_Pin|Digit2_Blanking_Pin|Digit3_Strobe_Pin
+                          |Digit4_Heatwire_Pin|Digit5_Pin|Digit6_Pin|Digit7_Pin
+                          |Digit8_Pin|Digit9_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : PB2 */
+  GPIO_InitStruct.Pin = GPIO_PIN_2;
+  GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PBPin PBPin PBPin PBPin
+                           PBPin PBPin */
+  GPIO_InitStruct.Pin = Tube0_Pin|Tube1_Pin|Tube2_Pin|Tube4_Pin
+                          |LeftComma_Pin|RightComma_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PA10 PA11 PA12 PA15 */
+  GPIO_InitStruct.Pin = GPIO_PIN_10|GPIO_PIN_11|GPIO_PIN_12|GPIO_PIN_15;
+  GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure peripheral I/O remapping */
+  __HAL_AFIO_REMAP_PD01_ENABLE();
+
+}
+
+/* USER CODE BEGIN 2 */
+
+/* USER CODE END 2 */
