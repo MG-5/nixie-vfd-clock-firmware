3522d7fc27437c023ac7dbbfb1d7bee3d60cbcb2
MG-5 <MG-5@users.noreply.github.com>
2025-10-29 01:20:45 +0100

    Link fault handler to FreeRTOS configASSERT

--- cubemx/Core/Inc/FreeRTOSConfig.h
+++ cubemx/Core/Inc/FreeRTOSConfig.h
@@ -43,7 +43,7 @@
  *----------------------------------------------------------*/
 
 /* USER CODE BEGIN Includes */
-/* Section where include file can be added */
+#include "../../../../core/inc/core/fault_handler.h"
 /* USER CODE END Includes */
 
 /* Ensure definitions are only used by the compiler, and not by the assembler. */
@@ -129,7 +129,7 @@ See http://www.FreeRTOS.org/RTOS-Cortex-M3-M4.html. */
 /* Normal assert() semantics without relying on the provision of an assert.h
 header file. */
 /* USER CODE BEGIN 1 */
-#define configASSERT( x ) if ((x) == 0) {taskDISABLE_INTERRUPTS(); for( ;; );}
+#define configASSERT( x ) if ((x) == 0) {faultHandler();}
 /* USER CODE END 1 */
 
 /* Definitions that map the FreeRTOS port interrupt handlers to their CMSIS
